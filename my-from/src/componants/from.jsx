335629a488997f87211deff45eda73cb576344ab
Mayur <mayurpmali7@gmail.com>
2023-12-23 15:31:21 +0530

    registration validation

--- my-from/src/componants/from.jsx
+++ my-from/src/componants/from.jsx
@@ -1,7 +1,9 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import {Link } from "react-router-dom";
 
-
+//site key : 6Ld2vjgpAAAAAB-cpCkxehmhmZpd-TCmevbbg4De
+//secret key: 6Ld2vjgpAAAAAOrqK3oEHtpokELb00UHjmte2YXM
 
 const LoginForm = ()=>{
     const init ={
@@ -46,6 +48,9 @@ const LoginForm = ()=>{
         dispatch({type:"update",fld:f,val:{value:v,valid:ch,touch:true,error:msg}});
     }
 
+    //for capcha
+    const[capVal,setCapVal] = useState(null);
+
     return(
         <div>
             <div className="container mt-5  col-md-4 bg-light needs-validation pb-5 pt-5">
@@ -81,8 +86,17 @@ const LoginForm = ()=>{
                     </div>
                 </div>
 
+
+                <div class="mt-3 ms-5 me-5 pt-4 ps-4">
+                    <ReCAPTCHA 
+                    sitekey="6Ld2vjgpAAAAAB-cpCkxehmhmZpd-TCmevbbg4De" 
+                    onChange={(e)=>{
+                        setCapVal(e);
+                    }}></ReCAPTCHA>
+                </div>
+
                 <div class="mt-3 d-grid gap-2 ms-5 me-5 pt-4">
-                    <button className={cust.username.valid&&cust.password.valid ? "btn btn-primary":"btn btn-primary disabled"}>Login</button>
+                    <button className={cust.username.valid&&cust.password.valid&&capVal!=null ? "btn btn-primary":"btn btn-primary disabled"}>Login</button>
                 </div>
 
             </div>
@@ -91,6 +105,124 @@ const LoginForm = ()=>{
 }
 
 const RegistrationForm = ()=>{
+
+    const init ={
+        firstname:{value:"",valid:false, touch:false, error:""},
+        lastname:{value:"",valid:false, touch:false, error:""},
+        email:{value:"",valid:false, touch:false, error:""},
+        mobileno: {value:"",valid:false, touch:false, error:""},
+        username :{value:"",valid:false, touch:false, error:""},
+        password :{value:"",valid:false, touch:false, error:""},
+        confirmpassword :{value:"",valid:false, touch:false, error:""}
+    }
+
+    const reducer = (state,action)=>{
+        if(action.type==="update"){
+            return {...state,[action.fld]:action.val};
+        }
+        else{
+            return init;
+        }
+    }
+
+    const [cust,dispatch] = useReducer(reducer,init);
+    // const [email1,setEmail] = useState("");
+    // const [username,setUsername] = useState("");
+
+    //for button
+    // const [bt,setbt] = useState(false);
+
+    //for cpatcha
+    const[capVal,setCapVal] = useState(null);
+    const checkValid = (fld,v)=>{
+        var ch = true;
+        var msg = "Looks good!";
+        switch(fld){
+            case "firstname":
+                var exp1 = /^[A-Z]{1}[a-z]{1,15}$/;
+                if(!exp1.test(v)){
+                    ch = false;
+                    msg = 'First character must be capital and rest are small.'
+                }
+                break;
+            case "lastname":
+                var exp2 = /^[A-Z]{1}[a-z]{1,15}$/;
+                if(!exp2.test(v)){
+                    ch = false;
+                    msg = 'First character must be capital and rest are small.';
+                }
+                break;
+            case "email":
+                var exp3 = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+                // var url = "http://localhost:9000/valid_email?email=%27"+v+"%27";
+                // console.log(url);
+                // fetch(url).then((resp)=>{return resp.text()}).then((data)=>{setEmail(data)});
+                // console.log(email);
+                if(!exp3.test(v)){
+                    ch=false;
+                    msg = "Please enter valid email."
+                }
+                // else if(exp3.test(v)){
+                //     var url = "http://localhost:9000/valid_email?email=%27"+v+"%27";
+                //     fetch(url).then((resp)=>{return resp.text()}).then((data)=>{
+                //         if(data=='false'){
+                //             ch=false;
+                //             msg = "Email already taken";
+                //         }
+                //     });
+                // }
+                break;
+            case "mobileno":
+                var exp4 = /^[0-9]{10}$/;
+                if(!exp4.test(v)){
+                    ch = false;
+                    msg = "Please enter valid mobile number."
+                }
+                break;
+            case "username":
+                var exp5  = /^[0-9a-zA-Z]{4,15}$/
+                // var url1 = "http://localhost:9000/valid_username?username="+v;
+                // fetch(url1).then((resp)=>{return resp.text()}).then((data)=>{setUsername(data)});
+                if(!exp5.test(v)){
+                    ch = false;
+                    msg = "Username should be between 4 to 15 characters and contains only digit and characters."
+                }
+                // if(username==="false"){
+                //     ch = false;
+                //     msg = "Username already taken.";
+                // }
+                break;
+            
+            case "password":
+                var exp6 = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/;
+                if(!exp6.test(v)){
+                    ch = false;
+                    msg = "Password must contain Minimum 8 and maximum 15 characters, at least one uppercase letter, one lowercase letter, one number and one special character."
+                }
+                if(cust.confirmpassword.value.length != 0){
+                    cust.confirmpassword.value = "";
+                    cust.confirmpassword.touch = false;
+                }
+                break;
+            case "confirmpassword":
+                // console.log(v);
+                console.log(cust.password)
+                if(cust.password.value != v){
+                    ch = false;
+                    msg = "Password mismatched."
+                }
+                break;
+            
+        }
+        // console.log(ch +" " +msg);
+        return {ch,msg};
+    }
+
+    const handleChange= (f,v)=>{
+        const{ch,msg} = checkValid(f,v);
+        dispatch({type:"update",fld:f,val:{value:v,valid:ch,touch:true,error:msg}});
+    }
+    
     return(
         <div>
             <div className="container mt-5  col-md-4 bg-light needs-validation pb-5 pt-5 rounded">
@@ -105,35 +237,103 @@ const RegistrationForm = ()=>{
 
                 <div className="row">
                     <div class="col-md">
-                        <label for="validationServer01" class="form-label">First name</label>
-                        <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First name" required/>
-                        <div class="valid-feedback">
-                        Looks good!
+                        <label  class="form-label">First name</label>
+                        <input type="text" className={cust.firstname.touch?(cust.firstname.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServer01" placeholder="First name" required
+                        value={cust.firstname.value}
+                        onChange={(e)=>{
+                            handleChange("firstname",e.target.value);
+                        }}/>
+                        <div className={cust.firstname.valid?"valid-feedback":"invalid-feedback"}>
+                            {cust.firstname.error}
                         </div>
                     </div>
                     <div class="col-md">
-                        <label for="validationServer02" class="form-label">Last name</label>
-                        <input type="text" class="form-control is-valid" id="validationServer02"  placeholder="Last name" required/>
-                        <div class="valid-feedback">
-                        Looks good!
+                        <label  class="form-label">Last name</label>
+                        <input type="text" className={cust.lastname.touch?(cust.lastname.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServer01" placeholder="First name" required
+                        value={cust.lastname.value}
+                        onChange={(e)=>{
+                            handleChange("lastname",e.target.value);
+                        }}/>
+                        <div className={cust.lastname.valid?"valid-feedback":"invalid-feedback"}>
+                            {cust.lastname.error}
                         </div>
                     </div>
                 </div>
 
-                <label for="validationServer01" class="form-label">Email</label>
-                <input type="email" class="form-control is-valid" id="validationServer01" value="email" placeholder="email" required/>
-                <div class="valid-feedback">
-                    Looks good!
+                <label  className="form-label">Email</label>
+                <input type="email" className={cust.email.touch?(cust.email.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServer01" placeholder="email" required
+                value={cust.email.value}
+                onChange={(e)=>{
+                    handleChange("email",e.target.value);
+                }}/>
+                <div className={cust.email.valid?"valid-feedback":"invalid-feedback"}>
+                    {cust.email.error}
+                </div>
+
+                <label className="form-label ">Mobile No:</label>
+                <div class="input-group has-validation">
+                    {/* <label for="validationServer01" class="form-label">Enter username</label> */}
+                    <span class="input-group-text" id="inputGroupPrepend3">+91</span>
+                    <input type="text" className={cust.mobileno.touch?(cust.mobileno.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServerUsername" aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" placeholder="Mobile No"required
+                    value={cust.mobileno.value}
+                    onChange={(e)=>{
+                        handleChange("mobileno",e.target.value);
+                    }}/>
+                    <div className={cust.mobileno.valid?"valid-feedback":"invalid-feedback"}>
+                        {cust.mobileno.error}
+                    </div>
+                </div>
+
+                <label  className="form-label ">Select username</label>
+                <div class="input-group has-validation">
+                    {/* <label for="validationServer01" class="form-label">Enter username</label> */}
+                    <span class="input-group-text" id="inputGroupPrepend3">@</span>
+                    <input type="text" className={cust.username.touch?(cust.username.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServerUsername" aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" placeholder="select username"required
+                    value={cust.username.value}
+                    onChange={(e)=>{
+                        handleChange("username",e.target.value);
+                    }}/>
+                    <div className={cust.username.valid?"valid-feedback":"invalid-feedback"}>
+                        {cust.username.error}
+                    </div>
+                </div>
+
+                <label className="form-label ">Password</label>
+                <div class="input-group has-validation">
+                    {/* <label for="validationServer01" class="form-label">Password</label> */}
+                    <input type="password" className={cust.password.touch?(cust.password.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServer01" placeholder="Password"required
+                    value={cust.password.value} onChange={(e)=>{handleChange("password",e.target.value)}}/>
+                    <div className={cust.password.valid?"valid-feedback":"invalid-feedback"}>
+                        {cust.password.error}
+                    </div>
+                </div>
+
+                <label  className="form-label">Confirm Password</label>
+                <div class="input-group has-validation">
+                    {/* <label for="validationServer01" class="form-label">Password</label> */}
+                    <input type="password" className={cust.confirmpassword.touch?(cust.confirmpassword.valid? "form-control is-valid":"form-control is-invalid"):"form-control"} id="validationServer01" placeholder="confirmpassword"required
+                    value={cust.confirmpassword.value} onChange={(e)=>{handleChange("confirmpassword",e.target.value)}}/>
+                    <div className={cust.confirmpassword.valid?"valid-feedback":"invalid-feedback"}>
+                        {cust.confirmpassword.error} 
+                    </div>
+                </div>
+
+                <div class="mt-3 ms-5 me-5 pt-4 ps-4">
+                    <ReCAPTCHA 
+                    sitekey="6Ld2vjgpAAAAAB-cpCkxehmhmZpd-TCmevbbg4De" 
+                    onChange={(e)=>{
+                        setCapVal(e);
+                    }}></ReCAPTCHA>
                 </div>
 
                 <div class="mt-3 d-grid gap-2 ms-5 me-5">
-                    <button class="btn btn-primary">Register</button>
+                    <button className={cust.firstname.value && cust.lastname.valid && cust.email.valid && cust.mobileno.valid 
+                        && cust.username.valid && cust.password.valid && cust.confirmpassword.valid && capVal != null? "btn btn-primary":"btn btn-primary disabled"}>Register</button>
                 </div>
 
             </div>
         </div>
     )
 }
-
 
 export {LoginForm,RegistrationForm}
